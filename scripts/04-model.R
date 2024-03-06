c94cb2083ea8e96eb512fd586c537f221ae29deb
faralk <156137245+faralk@users.noreply.github.com>
2024-03-05 20:21:25 -0500

    Get diagnostics working

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -34,4 +34,21 @@ saveRDS(
   file = "models/first_model.rds"
 )
 
+## Second model
+
+second_model <-
+  stan_glm(
+    formula = flying_time ~ length,
+    data = analysis_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 853
+  )
+
+saveRDS(
+  second_model,
+  file = "models/second_model.rds"
+)
 
